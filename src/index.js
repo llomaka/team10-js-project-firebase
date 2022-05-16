9fd25eaf0256034fb57f7f3b35bd76c45a430f37
Lilia Lomaka <llomaka80@gmail.com>
2022-05-16 21:24:51 +0300

    Спроба підключення автентифікації

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ import onLoaderVisible from './js/onLoaderVisible';
 import renderMainPage from './js/renderMainPage';
 import './sass/main.scss';
 import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signOut, createUserWithEmailAndPassword, connectAuthEmulator, sign } from 'firebase/auth';
 import { getFirestore, collection } from 'firebase/firestore';
+import {
+  hideLoginError,
+  showLoginState,
+  showLoginForm,
+  showApp,
+  showLoginError,
+  btnLogin,
+  btnSignup,
+  btnLogout
+} from './js/ui';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAz5NnVTxsmxCeYZqtfnfTHMShB31Yek1s",
@@ -21,14 +32,68 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-onAuthStateChanged(auth, user => {
-  if (user !== null) {
-    console.log('logged in!');
-  } else {
-    console.log('No user');
-  }
-});
 const currentFilmStorage = collection(db, 'currentFilms');
+// Login using email/password
+const loginEmailPassword = async () => {
+  const loginEmail = txtEmail.value;
+  const loginPassword = txtPassword.value;
+
+  // step 1: try doing this w/o error handling, and then add try/catch
+  await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+
+  // step 2: add error handling
+  // try {
+  //   await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  // }
+  // catch(error) {
+  //   console.log(`There was an error: ${error}`)
+  //   showLoginError(error)
+  // }
+}
+
+// Create new account using email/password
+const createAccount = async () => {
+  const email = txtEmail.value;
+  const password = txtPassword.value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`);
+    showLoginError(error);
+  }
+}
+
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user);
+      showApp();
+      showLoginState(user);
+
+      hideLoginError();
+      hideLinkError();
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`;
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogin.addEventListener("click", loginEmailPassword);
+btnSignup.addEventListener("click", createAccount);
+btnLogout.addEventListener("click", logout);
+
+connectAuthEmulator(auth, "https://localhost:1236");
+monitorAuthState();
 
 const refs = getRefs();
 
